623651fbb7865e2a5db538779cd992ef9b2df3fa
01Dri <drikill01@gmail.com>
2023-05-17 23:40:33 -0300

    Começando novo projeto REST

---
+++ ApiRestProjetct/src/main/java/me/dri/restproject/ApiRestProjetctApplication.java
@@ -0,0 +1,13 @@
+package me.dri.restproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProjetctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProjetctApplication.class, args);
+	}
+
+}
---
+++ ApiRestProjetct/src/test/java/me/dri/restproject/ApiRestProjetctApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dri.restproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestProjetctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
